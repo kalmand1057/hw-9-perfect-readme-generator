d98a44291238c8a683fe54fdcbfc7185e204e12c
Kalena Almand <kalmand1057@gmail.com>
2020-09-23 02:30:14 -0500

    added names of prompts for user

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,45 @@
 // array of questions for user
 const questions = [
-
+    {
+        type:,
+        message:,
+        name: "description",
+    },
+    {
+        type:,
+        message:,
+        name: "contents",
+    },
+    {
+        type:,
+        message:,
+        name: "installation",
+    },
+    {
+        type:,
+        message:,
+        name: "usage",
+    },
+    {
+        type:,
+        message:,
+        name: "license",
+    },
+    {
+        type:,
+        message:,
+        name: "contributing",
+    },
+    {
+        type:,
+        message:,
+        name: "tests",
+    },
+    {
+        type:,
+        message:,
+        name: "questions",
+    }
 ];
 
 // function to write README file
